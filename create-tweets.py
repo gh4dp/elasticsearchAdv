7ab30fd21838e8dccbb7560262648e6ae90a86e4
Dhimant Patel <drp4kri@gmail.com>
2020-05-10 17:49:41 -0400

    Fixed issues, now both json and tab files will provide same data.

--- create-tweets.py
+++ create-tweets.py
@@ -12,10 +12,11 @@ occupations = [ fkr.job() for x in range(7) ]
 mindate = datetime.date(1968,1,1)
 dateToday = datetime.datetime.now()
 
-dates15 = [ mindate+datetime.timedelta(days = random.randint(-100,100) ) for x in range(15) ]
+dates15 = [ mindate+datetime.timedelta(days = random.randint(-1000,1000) ) for x in range(15) ]
 users15 = [ fnames[x][0] + lnames[x] for x in range(15) ]
 names15 = [ fnames[x] + ', ' + lnames[x] for x in range(15) ]
 ages15 = [ relativedelta(dateToday, dates15[x]).years for x in range(15)  ]
+aHeaderLine = "tweetid" + "\t" + "user" + "\t" + "post_date" + "\t" + "message" + "\t" + "name" + "\t"+ "age"
 
 
 if __name__ == "__main__":
@@ -24,19 +25,18 @@ if __name__ == "__main__":
 
     fw=open('tweets.json','w')
     fwt = open('tweets.txt', 'w')
+    fwt.write(aHeaderLine)
+    fwt.write("\n")
+
+
     for x in range(1000):    
         randompick = random.randint(0,14)
         postdate = dateToday + datetime.timedelta(days = random.randint(-100,0) )
         loremdata = fkr.paragraph()
-        aLine = "user:" + users15[randompick] + "\t" + "post_date:" + postdate.strftime('%Y-%m-%d') + "\t"
+        indexid = x+1000
+        aLine = str(indexid) + "\t" + users15[randompick] + "\t" + postdate.strftime('%Y-%m-%d') + "\t" + loremdata + "\t" + names15[randompick] + "\t"+ str(ages15[randompick])
 
-        """
-        "post_date:" + postdate.strftime('%Y-%m-%d') + "\t" +
-        "message:" + loremdata + "\t" + 
-        "name:" + names15[randompick] + "\t"+ 
-        "age:" + str(ages15[randompick])
-        """
-        fw.write(indexLinePrefix + str(x) + indexLineSuffix)
+        fw.write(indexLinePrefix + str(indexid) + indexLineSuffix)
         fw.write("\n")
         fw.write(json.dumps( {
             "user" : users15[randompick],
@@ -48,6 +48,7 @@ if __name__ == "__main__":
         ))
         fw.write("\n")
         fwt.write(aLine)
+        fwt.write("\n")
     fw.close()        
     fwt.close()
 
